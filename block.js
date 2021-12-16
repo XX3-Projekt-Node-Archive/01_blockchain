73eca84a4bfd468c3b6da3e04e47160c4615aec2
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-12-16 09:50:44 +0000

    mineBlock() 1

--- block.js
+++ block.js
@@ -21,6 +21,17 @@ static genesis(){
     return new this("Genesis time","----","abcd#123",[]);
 }
 
+static mineBlock(lastBlock,data){ // weitere Blocks in der Chain
+    const timestamp = Date.now(); // Zeit im ms seit 01.01.1970
+    const lastHash = lastBlock.hash;
+    const hash = "toDo HASH";
+
+    return new this(timestamp,lastHash,hash,data);
+}
+    
+
+
+
 
 
 }
--- dev-test.js
+++ dev-test.js
@@ -3,5 +3,7 @@ const Block = require("./block");
 // const block = new Block("time","hash1","hash2","data");
 // console.log(block.toString());
 
-console.log(Block.genesis().toString());
+//console.log(Block.genesis().toString());
 
+const testBlock = Block.mineBlock(Block.genesis(),"test");
+console.log(testBlock.toString());
